ca826ec8dac4ac359cdd8f3cc9154fb4655f4453
Antonio Miguel Pérez Rivera <antoniom.p.rivera@gmail.com>
2016-06-02 16:31:46 +0200

    Creado config.sh

---
+++ config.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+git clone https://github.com/IngenieriaWebUCA/iw2016-uJob.git
+cd iw2016-uJob
+sudo chmod 777 ./config.sh
+sudo chmod 777 ./inicio.sh
+echo Iniciando configuracion de la aplicacion web
+read -p "Nombre de usuario con permisos de administrador de la base de datos:" usuario
+read -s -p "Contrasena del usuario:" contrasenna
+mysql -u $usuario -p $contrasenna < iw2016ujob.sql
+echo Gracias por utilizar la herramienta
+./inicio.sh
