7c9d575ff3915565a1b808e981a46b58a60e4f3a
Antonio Miguel Pérez Rivera <antoniom.p.rivera@gmail.com>
2016-06-02 17:03:02 +0200

    Update config.sh

--- config.sh
+++ config.sh
@@ -4,7 +4,6 @@ sudo chmod 777 ./config.sh
 sudo chmod 777 ./inicio.sh
 echo Iniciando configuracion de la aplicacion web
 read -p "Nombre de usuario con permisos de administrador de la base de datos:" usuario
-read -p "Contrasena del usuario:" contrasenna
-mysql -u $usuario -p $contrasenna < iw2016ujob.sql
+mysql -u $usuario -p < iw2016ujob.sql
 echo Gracias por utilizar la herramienta
 ./inicio.sh
